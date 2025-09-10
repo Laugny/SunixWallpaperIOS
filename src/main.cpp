ececcf12f204e0ec7a5c85567ee5bb938645e1a6
Laugny <79530050+Laugny@users.noreply.github.com>
2025-09-10 15:42:08 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,17 @@
 #include <Geode/Modify/LevelEditorLayer.hpp>
 #include <Geode/Modify/CCLayer.hpp>
 #include <Geode/Geode.hpp>
-#include <vector>
 
 using namespace geode::prelude;
 
-// Flag: sind wir im Level oder Editor?
+// Flag, ob wir gerade im Level oder Editor sind
 static bool gInLevelOrEditor = false;
 
-// Hintergrund-Emitter
+// Hintergrund-Node & Partikel-Emitter
 static CCParallaxNodeExtra* gBG = nullptr;
 static CCParticleSystemQuad* gEmitter = nullptr;
 
-// Hilfsfunktionen
+// Hilfsfunktionen zur Verwaltung von Hintergrund & Emitter
 static void ensureBG(CCLayer* layer, int zOrder) {
     if (!gBG) {
         gBG = CCParallaxNodeExtra::create();
@@ -28,38 +27,22 @@ static void replaceBG(CCLayer* layer) {
     gBG = nullptr;
 
     auto tree = CCSprite::create(Mod::get()->getResource("res/tree.png")->c_str());
-    auto sun = CCSprite::create(Mod::get()->getResource("res/sun.png")->c_str());
+    auto sun  = CCSprite::create(Mod::get()->getResource("res/sun.png")->c_str());
 
     auto win = CCDirector::sharedDirector()->getWinSize();
     tree->setScaleY(win.height / tree->getContentSize().height);
-    tree->setPosition({tree->getContentSize().width * 0.5f, 0.0f});
+    tree->setPosition({ tree->getContentSize().width * 0.5f, 0.0f });
     sun->setPosition({ win.width / 2, win.height * 0.85f });
 
     ensureBG(layer, -10);
-    gBG->addChild(sun, 1, {0.7f, 0.75f}, {sun->getPosition().x, sun->getPosition().y});
-    gBG->addChild(tree, 1, {1.0f, 0.25f}, {tree->getPosition().x, tree->getPosition().y});
+    gBG->addChild(sun, 1, {0.7f, 0.75f}, sun->getPosition());
+    gBG->addChild(tree, 1, {1.0f, 0.25f}, tree->getPosition());
 }
 
 static void attachEmitterToMenuBG(CCLayer* layer) {
-    if (gEmitter->getParent() == layer) return;
-    layer->addChild(gEmitter, 0);
-}
-
-$execute(AttachEmitterToScene, CCLayer*, layer) {
-    if (gInLevelOrEditor) return;
-    if (!gEmitter) {
-        auto plist = Mod::get()->getResource("res/particleEffect.plist").unwrap();
-        auto dict = CCDictionary::createWithContentsOfFile(plist.c_str());
-        gEmitter = CCParticleSystemQuad::create();
-        if (gEmitter->initWithDictionary(dict, "")) {
-            gEmitter->setPositionType(kCCPositionTypeGrouped);
-            gEmitter->setAutoRemoveOnFinish(false);
-            gEmitter->setBlendAdditive(true);
-            auto win = CCDirector::sharedDirector()->getWinSize();
-            gEmitter->setPosition({ win.width * 0.5f, 0.0f });
-        }
+    if (gEmitter && gEmitter->getParent() != layer) {
+        layer->addChild(gEmitter, 0);
     }
-    attachEmitterToMenuBG(layer);
 }
 
 static void cleanupEmitter() {
@@ -81,12 +64,14 @@ class $modify(CCDirector) {
                   gInLevelOrEditor ? "SUPPRESSED" : "ENABLED");
 
         if (auto layer = getChildOfType<CCLayer>(scene, 0)) {
-            if (!dynamic_cast<SecretLayer*>(layer)
-                && !dynamic_cast<SecretLayer2*>(layer)
-                && !dynamic_cast<SecretLayer3*>(layer)
-                && !dynamic_cast<SecretLayer4*>(layer)
-                && !dynamic_cast<SecretRewardsLayer*>(layer)
-                && !dynamic_cast<GauntletSelectLayer*>(layer)) {
+            bool isSecret = dynamic_cast<SecretLayer*>(layer)
+                         || dynamic_cast<SecretLayer2*>(layer)
+                         || dynamic_cast<SecretLayer3*>(layer)
+                         || dynamic_cast<SecretLayer4*>(layer)
+                         || dynamic_cast<SecretRewardsLayer*>(layer)
+                         || dynamic_cast<GauntletSelectLayer*>(layer);
+
+            if (!isSecret) {
                 if (!gInLevelOrEditor) {
                     replaceBG(layer);
                 } else {
@@ -98,20 +83,22 @@ class $modify(CCDirector) {
 
     void reshapeProjection(const CCSize& newSize) {
         CCDirector::reshapeProjection(newSize);
-        if (gEmitter) {
-            const auto win = CCDirector::sharedDirector()->getWinSize();
-            gEmitter->setPosition(ccp(win.width * 0.5f, 0.0f));
-            gEmitter->setPosVar(ccp(win.width * 0.5f, 0.0f));
-            if (!gInLevelOrEditor) {
-                if (auto scene = getRunningScene()) {
-                    if (auto layer = getChildOfType<CCLayer>(scene, 0)) {
-                        ensureBG(layer, -10);
-                        attachEmitterToMenuBG(layer);
-                    }
+        if (!gEmitter) return;
+
+        const auto win = CCDirector::sharedDirector()->getWinSize();
+        gEmitter->setPosition(ccp(win.width * 0.5f, 0.f));
+        gEmitter->setPosVar(ccp(win.width * 0.5f, 0.f));
+
+        if (!gInLevelOrEditor) {
+            if (auto scene = getRunningScene()) {
+                if (auto layer = getChildOfType<CCLayer>(scene, 0)) {
+                    ensureBG(layer, -10);
+                    attachEmitterToMenuBG(layer);
                 }
             }
-            gEmitter->resumeSchedulerAndActions();
-            gEmitter->setVisible(!gInLevelOrEditor);
         }
+
+        gEmitter->resumeSchedulerAndActions();
+        gEmitter->setVisible(!gInLevelOrEditor);
     }
 };
